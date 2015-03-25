bc5d25d644d32b85555f04273aaf3d64288a347f
juanfrc <juanfrc2@gmail.com>
2015-03-24 19:07:46 -0500

    Added basic bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :passengers
   end
 
-  root 'airlines#index'
+  root 'flights#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
